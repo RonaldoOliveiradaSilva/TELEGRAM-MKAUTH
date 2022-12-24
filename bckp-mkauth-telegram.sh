6af28003956df1fef511c1558619fd5ceef2d705
Ciro Moniz <cirinho@canalbahia.net>
2022-12-23 22:13:01 -0300

    Adiciona compactação do arquivo de backup - resolve #1
    Adiciona compactação do arquivo de backup;
    
    Opção de escolher entre zip e tar;
    
    Atualização do nome do arquivo de backup incluindo extensão da compactação;

--- bckp-mkauth-telegram.sh
+++ bckp-mkauth-telegram.sh
@@ -6,6 +6,9 @@ Token="000000000:0000000000000-0000000000000000000000000000000"
 ChatID="-1234567890123"
 DirArqBkp="/opt/mk-auth/bckp/"
 
+# Formato de compactação desejado: zip ou tar
+Compactacao="zip"
+
 
 
 # Não alterar mais nada abaixo 
@@ -22,6 +25,15 @@ if [ -d "$DirArqBkp" ]; then
 
 # Seleciona o último arquivo de backup criado
 ArqBkp=$(ls -t "$DirArqBkp" | head -1)
+
+# Compacta o arquivo de backup
+if [ "$Compactacao" = "zip" ]; then
+  zip -r "$DirArqBkp/$ArqBkp.zip" "$DirArqBkp/$ArqBkp"
+  ArqBkp="$ArqBkp.zip"
+else
+  tar -zcvf "$DirArqBkp/$ArqBkp.tar.gz" "$DirArqBkp/$ArqBkp"
+  ArqBkp="$ArqBkp.tar.gz"
+fi
 
 # Verifica se o arquivo existe
 if [ -a "$DirArqBkp/$ArqBkp" ]; then
